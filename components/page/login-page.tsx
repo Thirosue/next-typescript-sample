a0e37ee4654575fecfd889ea68a3b6c553ec19d3
takeshi.hirosue <mirrored1976@gmail.com>
2021-08-19 08:58:39 +0900

    refactor: split login page compornents

---
+++ components/page/login-page.tsx
@@ -0,0 +1,169 @@
+import { useContext } from 'react'
+import Head from 'next/head'
+import { toast } from 'react-toastify'
+import { NextRouter, useRouter } from 'next/router'
+import { useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as yup from 'yup'
+import { AxiosPromise, AxiosResponse } from 'axios'
+import { useMutation } from 'react-query'
+import GlobalContext from '../../context/global-context'
+import Logo from '../../components/logo'
+import {
+  FormLabel,
+  FormErrorMessage,
+  Button,
+  Link,
+  Typography,
+} from '../../components/atoms'
+import { Progress } from '../../components/progress'
+import { TextFieldType } from '../../data'
+import {
+  AuthRequest,
+  AuthResponse,
+  AuthRepository,
+} from '../../repository/auth-repository'
+
+const captains = console
+
+type FormValues = {
+  email: string
+  password: string
+  rememberMe: boolean
+}
+
+const schema = yup.object().shape({
+  email: yup
+    .string()
+    .required('入力してください')
+    .email('メールアドレスを入力してください'),
+  password: yup
+    .string()
+    .required('入力してください')
+    .matches(
+      /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/,
+      'アルファベット（大文字小文字混在）と数字と特殊記号を組み合わせて8文字以上で入力してください'
+    ),
+})
+
+export const LoginPage = ({
+  passwordModalOpen,
+}: {
+  passwordModalOpen: () => void
+}): JSX.Element => {
+  const router: NextRouter = useRouter()
+  const mutation = useMutation(
+    (req: AuthRequest): AxiosPromise<AuthResponse> => AuthRepository.signIn(req)
+  )
+  const context = useContext(GlobalContext)
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormValues>({
+    resolver: yupResolver(schema),
+    defaultValues: {
+      email: 'test@test.com',
+      password: 'Password1?',
+    },
+  })
+
+  const doSubmit = (data: FormValues): void => {
+    captains.log(data)
+    const authRequest: AuthRequest = {
+      id: data.email,
+      password: data.password,
+    }
+    mutation.mutate(authRequest, {
+      onSuccess: (res: AxiosResponse<AuthResponse>) => {
+        context.updateState({
+          session: {
+            username: data.email,
+            jwtToken: res.data.token,
+            sub: 'sub',
+          },
+        })
+        router.push('/')
+        toast.success('ログインしました')
+      },
+    })
+  }
+
+  return (
+    <>
+      <Progress processing={mutation.isLoading} />
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+        <meta name="referrer" content="always" />
+      </Head>
+      <div className="flex justify-center items-center h-screen bg-gray-200 px-6">
+        <div className="p-6 max-w-sm w-full bg-white shadow-md rounded-md">
+          <div className="flex justify-center items-center">
+            <Logo />
+            <Typography variant="h4">Dashboard</Typography>
+          </div>
+
+          <form className="mt-4" onSubmit={handleSubmit(doSubmit)}>
+            <label className="block">
+              <FormLabel>Email</FormLabel>
+              <input
+                id="email"
+                type={TextFieldType.Email}
+                className={`mt-1 w-full border-gray-300 block rounded-md focus:border-indigo-600' ${
+                  errors.email ? 'border-red-400' : ''
+                }`}
+                {...register('email')}
+              />
+              <FormErrorMessage>{errors.email?.message}</FormErrorMessage>
+            </label>
+
+            <label className="block mt-3">
+              <FormLabel>Password</FormLabel>
+              <input
+                id="password"
+                type={TextFieldType.Password}
+                className={`mt-1 w-full border-gray-300 block rounded-md focus:border-indigo-600' ${
+                  errors.password ? 'border-red-400' : ''
+                }`}
+                {...register('password')}
+              />
+              <FormErrorMessage>{errors.password?.message}</FormErrorMessage>
+            </label>
+
+            <div className="flex justify-between items-center mt-4">
+              <div>
+                <label className="inline-flex items-center">
+                  <input
+                    type="checkbox"
+                    className="form-checkbox text-indigo-600"
+                    {...register('rememberMe')}
+                  />
+                  <Typography variant={'body2'} classes={['mx-2']}>
+                    Remember me
+                  </Typography>
+                </label>
+              </div>
+
+              <div>
+                <Link onClick={passwordModalOpen}>Forgot your password?</Link>
+              </div>
+            </div>
+
+            <div className="mt-6">
+              <Button color={'primary'} fullWidth={true}>
+                Sign in
+              </Button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default LoginPage
--- pages/login.tsx
+++ pages/login.tsx
@@ -1,95 +1,11 @@
-import { useState, useContext, ReactElement } from 'react'
+import { useState, ReactElement } from 'react'
 import { Many } from 'lodash'
-import Head from 'next/head'
-import { toast } from 'react-toastify'
-import { NextRouter, useRouter } from 'next/router'
-import { useForm } from 'react-hook-form'
-import { yupResolver } from '@hookform/resolvers/yup'
-import * as yup from 'yup'
-import { AxiosPromise, AxiosResponse } from 'axios'
-import { useMutation } from 'react-query'
-import GlobalContext from '../context/global-context'
-import Logo from '../components/logo'
-import {
-  FormLabel,
-  FormErrorMessage,
-  Button,
-  Link,
-  Typography,
-} from '../components/atoms'
 import { SimpleLayout } from '../components/template'
-import { Progress } from '../components/progress'
+import LoginPage from '../components/page/login-page'
 import PasswordDialog from '../components/page/password-dialog'
 import ConfirmCodeModal from '../components/page/confirm-code-dialog'
-import { TextFieldType } from '../data'
-import {
-  AuthRequest,
-  AuthResponse,
-  AuthRepository,
-} from '../repository/auth-repository'
-
-const captains = console
-
-type FormValues = {
-  email: string
-  password: string
-  rememberMe: boolean
-}
-
-const schema = yup.object().shape({
-  email: yup
-    .string()
-    .required('入力してください')
-    .email('メールアドレスを入力してください'),
-  password: yup
-    .string()
-    .required('入力してください')
-    .matches(
-      /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/,
-      'アルファベット（大文字小文字混在）と数字と特殊記号を組み合わせて8文字以上で入力してください'
-    ),
-})
 
 export default function Login(): JSX.Element {
-  const router: NextRouter = useRouter()
-  const mutation = useMutation(
-    (req: AuthRequest): AxiosPromise<AuthResponse> => AuthRepository.signIn(req)
-  )
-  const context = useContext(GlobalContext)
-
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<FormValues>({
-    resolver: yupResolver(schema),
-    defaultValues: {
-      email: 'test@test.com',
-      password: 'Password1?',
-    },
-  })
-
-  const doSubmit = (data: FormValues): void => {
-    captains.log(data)
-    const authRequest: AuthRequest = {
-      id: data.email,
-      password: data.password,
-    }
-    mutation.mutate(authRequest, {
-      onSuccess: (res: AxiosResponse<AuthResponse>) => {
-        context.updateState({
-          session: {
-            username: data.email,
-            jwtToken: res.data.token,
-            sub: 'sub',
-          },
-        })
-        router.push('/')
-        toast.success('ログインしました')
-      },
-    })
-  }
-
   // For Password Modal
   const [state, setState] =
     useState<Many<'Init' | 'WaitingForCode' | 'Done'>>('Init')
@@ -100,78 +16,7 @@ export default function Login(): JSX.Element {
 
   return (
     <>
-      <Progress processing={mutation.isLoading} />
-      <Head>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, shrink-to-fit=no"
-        />
-        <meta httpEquiv="x-ua-compatible" content="ie=edge" />
-        <meta name="referrer" content="always" />
-      </Head>
-      <div className="flex justify-center items-center h-screen bg-gray-200 px-6">
-        <div className="p-6 max-w-sm w-full bg-white shadow-md rounded-md">
-          <div className="flex justify-center items-center">
-            <Logo />
-            <Typography variant="h4">Dashboard</Typography>
-          </div>
-
-          <form className="mt-4" onSubmit={handleSubmit(doSubmit)}>
-            <label className="block">
-              <FormLabel>Email</FormLabel>
-              <input
-                id="email"
-                type={TextFieldType.Email}
-                className={`mt-1 w-full border-gray-300 block rounded-md focus:border-indigo-600' ${
-                  errors.email ? 'border-red-400' : ''
-                }`}
-                {...register('email')}
-              />
-              <FormErrorMessage>{errors.email?.message}</FormErrorMessage>
-            </label>
-
-            <label className="block mt-3">
-              <FormLabel>Password</FormLabel>
-              <input
-                id="password"
-                type={TextFieldType.Password}
-                className={`mt-1 w-full border-gray-300 block rounded-md focus:border-indigo-600' ${
-                  errors.password ? 'border-red-400' : ''
-                }`}
-                {...register('password')}
-              />
-              <FormErrorMessage>{errors.password?.message}</FormErrorMessage>
-            </label>
-
-            <div className="flex justify-between items-center mt-4">
-              <div>
-                <label className="inline-flex items-center">
-                  <input
-                    type="checkbox"
-                    className="form-checkbox text-indigo-600"
-                    {...register('rememberMe')}
-                  />
-                  <Typography variant={'body2'} classes={['mx-2']}>
-                    Remember me
-                  </Typography>
-                </label>
-              </div>
-
-              <div>
-                <Link onClick={() => setPasswordModalOpen(true)}>
-                  Forgot your password?
-                </Link>
-              </div>
-            </div>
-
-            <div className="mt-6">
-              <Button color={'primary'} fullWidth={true}>
-                Sign in
-              </Button>
-            </div>
-          </form>
-        </div>
-      </div>
+      <LoginPage passwordModalOpen={() => setPasswordModalOpen(true)} />
       {passwordModal && (
         <PasswordDialog
           onSubmit={() => setState('WaitingForCode')}
