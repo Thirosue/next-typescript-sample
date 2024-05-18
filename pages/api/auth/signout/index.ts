6da484e8bb1a9892dd3e58f0646e635640cbee12
takeshi.hirosue <takeshi.hirosue@bigtreetc.com>
2024-05-18 14:35:19 +0900

    chore: Update CORS headers to allow DELETE method in signout API endpoint

--- pages/api/auth/signout/index.ts
+++ pages/api/auth/signout/index.ts
@@ -3,7 +3,7 @@ import { NextApiRequest, NextApiResponse } from 'next'
 export default (req: NextApiRequest, res: NextApiResponse): void => {
   // CORSヘッダーの設定
   res.setHeader('Access-Control-Allow-Origin', '*') // すべてのオリジンからのアクセスを許可
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS') // 許可するHTTPメソッド
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, OPTIONS') // 許可するHTTPメソッド
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization') // 許可するヘッダー
 
   // OPTIONSメソッドのハンドリング（CORSプリフライトリクエスト）
